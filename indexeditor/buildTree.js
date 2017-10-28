06f4ff10af039397f76f64543516d827b266dd9f
Sascha Goldhofer <post@saschagoldhofer.de>
2017-10-28 14:50:39 +0200

    Update index-editor to not show hidden entries

--- indexeditor/buildTree.js
+++ indexeditor/buildTree.js
@@ -1,12 +1,17 @@
 const UISchema = require("editron-core/utils/UISchema");
 const UI_PROPERTY = UISchema.UI_PROPERTY;
 const gp = require("gson-pointer");
-
+// const isUrl = /(((https?|file):\/\/)|.{8}-.{4}-.{4}-.{4}-.{12})/;
 
 module.exports = function buildTree(pointer, data, controller, depth = 1) {
-    // @ui-option title
     const schema = controller.schema().get(pointer);
 
+    // @ui-option hidden
+    if (UISchema.isHidden(schema)) {
+        return undefined;
+    }
+
+    // @ui-option title
     let title = UISchema.resolveReference(pointer, controller, gp.get(schema, `#/${UI_PROPERTY}/title-overview`));
     title = UISchema.isEmpty(title) ? UISchema.getTitle(schema) : UISchema.sanitizeString(title);
     const icon = UISchema.getIcon(schema);
@@ -19,6 +24,7 @@ module.exports = function buildTree(pointer, data, controller, depth = 1) {
             editable: true,
             children: data
                 .map((item, index) => buildTree(`${pointer}/${index}`, data[index], controller, depth - 1))
+                .filter((value) => value !== undefined)
         };
     }
 
@@ -31,6 +37,7 @@ module.exports = function buildTree(pointer, data, controller, depth = 1) {
             children: Object
                 .keys(data)
                 .map((key) => buildTree(`${pointer}/${key}`, data[key], controller, depth - 1))
+                .filter((value) => value !== undefined)
         };
     }
 
